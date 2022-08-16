CatalaLang/catala-examples

commit 5fd8e09a8b5b320ab4d5b504426a0335720c0863

Remove age prompts, deduct from birth date correctly

examples/allocations_familiales/securite_sociale_D.catala_fr
@@ -376,7 +376,8 @@ champ d'application AllocationsFamiliales :
     (droit_ouvert_majoration de enfant) et
     prestations_familiales.régime_outre_mer_l751_1 et
     (nombre de enfants_à_charge_droit_ouvert_prestation_familiale = 1) et
-    (enfant.âge >= 11 et enfant.âge < 16)
+    (enfant.date_de_naissance +@ 11 an <=@ date_courante et
+     enfant.date_de_naissance +@ 16 an >@ date_courante)
   conséquence égal à
     bmaf.montant *€ 3,69 %
 
@@ -385,7 +386,7 @@ champ d'application AllocationsFamiliales :
     (droit_ouvert_majoration de enfant) et
     prestations_familiales.régime_outre_mer_l751_1 et
     (nombre de enfants_à_charge_droit_ouvert_prestation_familiale = 1) et
-    (enfant.âge >= 16)
+    (enfant.date_de_naissance +@ 16 an >@ date_courante)
   conséquence égal à
     bmaf.montant *€ 5,67 %
 ```

examples/aides_logement/code_construction_legislatif.catala_fr
@@ -202,7 +202,8 @@ exception cas_base_l822_4 règle condition_logement_location_tiers
     selon ménage.logement.loué_ou_sous_loué_à_des_tiers sous forme
     -- LouéOuSousLouéÀDesTiers.Non: vrai
     -- LouéOuSousLouéÀDesTiers.Oui de personne:
-      personne.age_personne_sous_location <= 30 ou
+      personne.date_naissance_personne_sous_location +@ 30 an >@
+        date_courante ou
       personne.conforme_article_l442_1
   conséquence rempli
 ```
@@ -932,7 +933,6 @@ champ d'application ÉligibilitéAllocationLogement:
         -- obligation_scolaire: enfant.EnfantÀCharge.obligation_scolaire
         -- rémuneration_mensuelle: enfant.EnfantÀCharge.rémuneration_mensuelle
         -- date_de_naissance: enfant.EnfantÀCharge.date_de_naissance
-        -- âge: enfant.EnfantÀCharge.âge
         -- prise_en_charge: (selon enfant.EnfantÀCharge.prise_en_charge sous forme
           -- PriseEnCharge.EffectiveEtPermanente:
             PriseEnChargeEnfant.EffectiveEtPermanente
@@ -967,7 +967,6 @@ champ d'application ÉligibilitéAllocationLogement:
         -- obligation_scolaire: enfant.EnfantÀCharge.obligation_scolaire
         -- rémuneration_mensuelle: enfant.EnfantÀCharge.rémuneration_mensuelle
         -- date_de_naissance: enfant.EnfantÀCharge.date_de_naissance
-        -- âge: enfant.EnfantÀCharge.âge
         -- prise_en_charge: (selon enfant.EnfantÀCharge.prise_en_charge sous forme
           -- PriseEnCharge.EffectiveEtPermanente:
             PriseEnChargeEnfant.EffectiveEtPermanente

examples/aides_logement/code_construction_reglementaire.catala_fr
@@ -1142,7 +1142,8 @@ l'article L. 823-2 du présent code ;
 champ d'application ÉligibilitéAidesPersonnelleLogement:
   règle prise_en_compte_personne_à_charge de personne_à_charge sous condition
     selon personne_à_charge sous forme
-    -- EnfantÀCharge de enfant: enfant.EnfantÀCharge.âge <= 21
+    -- EnfantÀCharge de enfant:
+      enfant.EnfantÀCharge.date_de_naissance +@ 21 an >@ date_courante
     -- AutrePersonneÀCharge de parent: faux
   conséquence rempli
 ```

examples/aides_logement/prologue.catala_fr
@@ -154,7 +154,7 @@ déclaration énumération ParentOuAutre:
   -- Autre
 
 déclaration structure PersonneSousLocation:
-  donnée age_personne_sous_location contenu entier
+  donnée date_naissance_personne_sous_location contenu date
   donnée conforme_article_l442_1 contenu booléen
 
 déclaration énumération LouéOuSousLouéÀDesTiers:
@@ -185,7 +185,6 @@ déclaration structure EnfantÀCharge:
   donnée rémuneration_mensuelle contenu argent
   donnée obligation_scolaire contenu SituationObligationScolaire
   donnée prise_en_charge contenu PriseEnCharge
-  donnée âge contenu entier
   donnée situation_garde_alternée contenu SituationGardeAlternée
 
 déclaration énumération Parenté:
@@ -237,7 +236,6 @@ déclaration structure Patrimoine:
 déclaration structure Demandeur:
   donnée satisfait_conditions_l512_2_code_sécurité_sociale
     contenu booléen
-  donnée age_demandeur contenu entier
   donnée date_naissance contenu date
   donnée nationalité contenu Nationalité
   donnée patrimoine contenu Patrimoine

examples/aides_logement/tests/tests_eligibilite_apl.catala_fr
@@ -22,7 +22,6 @@ champ d'application Exemple1 :
      EnfantÀCharge contenu (EnfantÀCharge {
        -- bénéficie_titre_personnel_aide_personnelle_logement : faux
        -- prise_en_charge : PriseEnCharge.EffectiveEtPermanente
-       -- âge : 19
        -- identifiant: 0
        -- a_déjà_ouvert_droit_aux_allocations_familiales: vrai
        -- date_de_naissance: |2001-01-01|
@@ -32,7 +31,6 @@ champ d'application Exemple1 :
      }); EnfantÀCharge contenu (EnfantÀCharge {
        -- bénéficie_titre_personnel_aide_personnelle_logement : faux
        -- prise_en_charge : PriseEnCharge.EffectiveEtPermanente
-       -- âge : 11
        -- identifiant: 1
        -- a_déjà_ouvert_droit_aux_allocations_familiales: vrai
        -- date_de_naissance: |2009-01-01|
@@ -42,7 +40,6 @@ champ d'application Exemple1 :
      }); EnfantÀCharge contenu (EnfantÀCharge {
        -- bénéficie_titre_personnel_aide_personnelle_logement : faux
        -- prise_en_charge : PriseEnCharge.EffectiveEtPermanente
-       -- âge : 8
        -- identifiant: 2
        -- a_déjà_ouvert_droit_aux_allocations_familiales: vrai
        -- date_de_naissance: |2012-01-01|
@@ -79,7 +76,6 @@ champ d'application Exemple1 :
   }
   définition éligibilité.demandeur égal à Demandeur {
     -- satisfait_conditions_l512_2_code_sécurité_sociale : vrai
-    -- age_demandeur : 52
     -- date_naissance : |1970-05-02|
     -- nationalité : Française
     -- patrimoine : Patrimoine {
@@ -106,7 +102,6 @@ champ d'application Exemple2 :
      EnfantÀCharge contenu (EnfantÀCharge {
        -- bénéficie_titre_personnel_aide_personnelle_logement : faux
        -- prise_en_charge : PriseEnCharge.EffectiveEtPermanente
-       -- âge : 6
        -- identifiant: 0
        -- a_déjà_ouvert_droit_aux_allocations_familiales: vrai
        -- date_de_naissance: |2016-01-01|
@@ -116,7 +111,6 @@ champ d'application Exemple2 :
      }); EnfantÀCharge contenu (EnfantÀCharge {
        -- bénéficie_titre_personnel_aide_personnelle_logement : faux
        -- prise_en_charge : PriseEnCharge.EffectiveEtPermanente
-       -- âge : 7
        -- identifiant: 1
        -- a_déjà_ouvert_droit_aux_allocations_familiales: vrai
        -- date_de_naissance: |2015-01-01|
@@ -153,7 +147,6 @@ champ d'application Exemple2 :
   }
   définition éligibilité.demandeur égal à Demandeur {
     -- satisfait_conditions_l512_2_code_sécurité_sociale : vrai
-    -- age_demandeur : 30
     -- date_naissance : |1992-01-01|
     -- nationalité : Française
     -- patrimoine : Patrimoine {

examples/allocations_familiales/epilogue.catala_fr
@@ -10,17 +10,16 @@ la fonction du bloc de code ci-dessous.
 ```catala-metadata
 champ d'application EnfantLePlusÂgé:
   définition le_plus_âgé  égal à
-    contenu maximum entier initial Enfant {
+    contenu minimum date initial Enfant {
       -- identifiant: -1
       -- obligation_scolaire: Pendant
       -- rémuneration_mensuelle: 0€
-      -- date_de_naissance: |1900-01-01|
-      -- âge: 0
+      -- date_de_naissance: |2999-12-31|
       -- prise_en_charge: EffectiveEtPermanente
       -- a_déjà_ouvert_droit_aux_allocations_familiales: faux
       -- bénéficie_titre_personnel_aide_personnelle_logement: faux
     }
-    pour potentiel_plus_âgé dans enfants de potentiel_plus_âgé.âge
+    pour potentiel_plus_âgé dans enfants de potentiel_plus_âgé.date_de_naissance
 
 
 champ d'application AllocationsFamiliales:
@@ -120,8 +119,6 @@ champ d'application InterfaceAllocationsFamiliales:
       -- rémuneration_mensuelle : enfant.d_rémuneration_mensuelle
       -- date_de_naissance : enfant.d_date_de_naissance
       -- prise_en_charge : enfant.d_prise_en_charge
-      -- âge: accès_année de
-        (|0000-01-01| +@ (i_date_courante -@ enfant.d_date_de_naissance))
       -- obligation_scolaire :
         (si enfant.d_date_de_naissance +@ 3 an >=@ i_date_courante alors
           Avant

examples/allocations_familiales/prologue.catala_fr
@@ -24,7 +24,6 @@ déclaration structure Enfant :
   donnée obligation_scolaire contenu SituationObligationScolaire
   donnée rémuneration_mensuelle contenu argent
   donnée date_de_naissance contenu date
-  donnée âge contenu entier
   donnée prise_en_charge contenu PriseEnCharge
   donnée a_déjà_ouvert_droit_aux_allocations_familiales contenu booléen
   donnée bénéficie_titre_personnel_aide_personnelle_logement contenu booléen
@@ -58,7 +57,7 @@ déclaration champ d'application PrestationsFamiliales:
   sortie droit_ouvert condition dépend de Enfant
   sortie conditions_hors_âge condition dépend de Enfant
   interne plafond_l512_3_2 contenu argent
-  sortie âge_l512_3_2 contenu entier
+  sortie âge_l512_3_2 contenu durée
   sortie régime_outre_mer_l751_1 condition
   entrée date_courante contenu date
   entrée prestation_courante contenu ÉlémentPrestationsFamiliales
@@ -74,7 +73,7 @@ champ d'application PrestationsFamiliales:
 
 ```catala-metadata
 déclaration champ d'application AllocationFamilialesAvril2008:
-  sortie âge_minimum_alinéa_1_l521_3 contenu entier
+  sortie âge_minimum_alinéa_1_l521_3 contenu durée
 
 déclaration champ d'application EnfantLePlusÂgé:
   entrée enfants contenu collection Enfant
@@ -144,7 +143,7 @@ déclaration champ d'application AllocationsFamiliales:
 
   # Plafonds, âges limites et autres constantes
   interne nombre_enfants_l521_1 contenu entier
-  interne âge_minimum_alinéa_1_l521_3 contenu entier dépend de Enfant
+  interne âge_minimum_alinéa_1_l521_3 contenu durée dépend de Enfant
   interne nombre_enfants_alinéa_2_l521_3 contenu entier
   interne est_enfant_le_plus_âgé contenu booléen dépend de Enfant
   interne plafond_I_d521_3 contenu argent

examples/allocations_familiales/securite_sociale_L.catala_fr
@@ -59,7 +59,7 @@ champ d'application PrestationsFamiliales :
   étiquette cas_base règle droit_ouvert de enfant sous condition
     enfant.obligation_scolaire sous forme Après et
     (enfant.rémuneration_mensuelle <=€ plafond_l512_3_2) et
-    (enfant.âge < âge_l512_3_2)
+    (enfant.date_de_naissance +@ âge_l512_3_2 >@ date_courante)
   conséquence rempli
 
   # On définit les conditions hors âge d'abord car elles
@@ -120,7 +120,8 @@ champ d'application AllocationsFamiliales :
     # Puisqu'un enfant ne garde un âge donné que pour une période d'un an,
     # cette condition assure que l'allocation ne peut être distribuée que pour
     # un an.
-    (enfant.âge = prestations_familiales.âge_l512_3_2) et
+    ((enfant.date_de_naissance +@ prestations_familiales.âge_l512_3_2) -@
+      date_courante <^ 365 jour) et
     (enfant.a_déjà_ouvert_droit_aux_allocations_familiales) et
     (prestations_familiales.conditions_hors_âge de enfant)
   conséquence rempli
@@ -297,7 +298,8 @@ champ d'application AllocationsFamiliales :
   règle droit_ouvert_majoration de enfant
   sous condition
     (non (est_enfant_le_plus_âgé de enfant)) et
-    (enfant.âge >= âge_minimum_alinéa_1_l521_3 de enfant)
+    (enfant.date_de_naissance +@ âge_minimum_alinéa_1_l521_3 de enfant <=@
+      date_courante)
   conséquence rempli
 ```
 
@@ -312,7 +314,8 @@ champ d'application AllocationsFamiliales :
   sous condition
     (nombre de enfants_à_charge_droit_ouvert_prestation_familiale >=
       nombre_enfants_alinéa_2_l521_3) et
-    (enfant.âge >= âge_minimum_alinéa_1_l521_3 de enfant)
+    (enfant.date_de_naissance +@ âge_minimum_alinéa_1_l521_3 de enfant <=@
+      date_courante)
   conséquence rempli
 ```
 

commit c1e87d5fc670b325baa45847249f301e45c5ffab

Fix bug introduced by 5fd8e09

examples/allocations_familiales/securite_sociale_D.catala_fr
@@ -386,7 +386,7 @@ champ d'application AllocationsFamiliales :
     (droit_ouvert_majoration de enfant) et
     prestations_familiales.régime_outre_mer_l751_1 et
     (nombre de enfants_à_charge_droit_ouvert_prestation_familiale = 1) et
-    (enfant.date_de_naissance +@ 16 an >@ date_courante)
+    (enfant.date_de_naissance +@ 16 an <=@ date_courante)
   conséquence égal à
     bmaf.montant *€ 5,67 %
 ```

examples/allocations_familiales/tests/output/tests_allocations_familiales.catala_fr.Test14.Interpret
@@ -1,0 +1,1 @@
+[RESULT] Computation successful!

examples/allocations_familiales/tests/tests_allocations_familiales.catala_fr
@@ -320,6 +320,32 @@ champ d'application Test13:
   définition f.i_résidence égal à Mayotte
   règle f.i_personne_charge_effective_permanente_est_parent rempli
   assertion f.i_montant_versé = 34,99€
+
+déclaration champ d'application Test14:
+  f champ d'application InterfaceAllocationsFamiliales
+
+champ d'application Test14:
+  définition f.i_enfants égal à [EnfantEntrée {
+    -- d_identifiant: 0
+    -- d_date_de_naissance: |2004-12-22|
+    -- d_rémuneration_mensuelle: 435€
+    -- d_prise_en_charge: EffectiveEtPermanente
+    -- d_a_déjà_ouvert_droit_aux_allocations_familiales: vrai
+    -- d_bénéficie_titre_personnel_aide_personnelle_logement: faux
+  };
+  EnfantEntrée {
+    -- d_identifiant: 1
+    -- d_date_de_naissance: |2001-12-05|
+    -- d_rémuneration_mensuelle: 1682€
+    -- d_prise_en_charge: GardeAlternéeAllocataireUnique
+    -- d_a_déjà_ouvert_droit_aux_allocations_familiales: vrai
+    -- d_bénéficie_titre_personnel_aide_personnelle_logement: faux
+  }]
+  définition f.i_ressources_ménage égal à 64033 €
+  définition f.i_date_courante égal à |2022-05-01|
+  définition f.i_résidence égal à Guadeloupe
+  règle f.i_personne_charge_effective_permanente_est_parent rempli
+  assertion f.i_montant_versé = 48,77€
 ```
 
 ```catala-test {id="Test1.Interpret"}
@@ -371,5 +397,9 @@ catala Interpret -s Test12
 ```
 
 ```catala-test {id="Test13.Interpret"}
+catala Interpret -s Test13
+```
+
+```catala-test {id="Test14.Interpret"}
 catala Interpret -s Test13
 ```